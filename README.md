add545baec01e9d9f9e1e6bcc0cf498b920511ee
PrimoSpade <93958328+PrimoSpade@users.noreply.github.com>
2022-02-14 18:44:34 +0800

    update final for main but still working on version 2 for js assessment(deadline 1900)

--- README.md
+++ README.md
@@ -8,20 +8,20 @@ You need node.js, npm and a terminal.
 
 ## Instructions
 
-Run the game with game with ' npm run main ' on your terminal while at the dictionary.
-Sometimes some of the random generated fields will be impossible to win. 
-(Still working on the version 2 (main1) to enable 100% clear chance)
+Run the game with game with ' npm run main ' on your terminal while at the dictionary. <br />
+Sometimes some of the random generated fields will be impossible to win.  <br />
+(Will still update version 2 (main1) after deadline(14Feb2022 1900) to improve it <br />
 Find your hat and have fun!
 
 ## Reflection
 
-**What did you like about this project?**
-It is very hard but was very fun when completed
+**What did you like about this project?** <br />
+It is very hard but was very fun when completed <br />
 
-**What did you struggle with in this project?**
-1st struggle during the start of the project as I have a idea but not sure where to start.
-2nd struggle was creating the holes and the hat without overlaps.
+**What did you struggle with in this project?** <br />
+1st struggle during the start of the project as I have a idea but not sure where to start. <br />
+2nd struggle was creating the holes and the hat without overlaps. <br />
 
-**What would make your experience with this assessment better?**
-More detail steps on what to do 1st and follows. 
-Self-improvement: Need to improve steps with my logic & how to add feature that can make game 100% non-blockage.
+**What would make your experience with this assessment better?** <br />
+More detail steps on what to do 1st and follows. <br />
+Self-improvement: Need to improve steps with my logic & how to add feature that can make game 100% non-blockage. <br />
---
+++ version2.js
@@ -0,0 +1,109 @@
+// Version 2 is about the same just that trying to shorten the code (npm run main1)
+// and to ensure the character will reach the hat without hole blocking (still working on this)
+
+const prompt = require("prompt-sync")({sigint: true});
+const clear = require("clear-screen");
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+class Field {
+    constructor(field = [[]]) {
+        this.field = field;
+        this.locationX = 0;
+        this.locationY = 0;
+        this.field[0][0] = pathCharacter;
+    }
+    static generateField(height, width, percentage = 0.1) {
+        const field = new Array(height).fill(0).map(el => new Array(width));
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                const prob = Math.random();
+                field[y][x] = prob > percentage ? fieldCharacter : hole;
+            }
+
+        }
+        const hatLocation = {
+            x: Math.floor(Math.random() * width),
+            y: Math.floor(Math.random() * height)
+        };
+        while (hatLocation.x == 0 && hatLocation.y == 0) {
+            hatLocation.x = Math.floor(Math.random() * width);
+            hatLocation.y = Math.floor(Math.random() * height);
+        }
+        field[hatLocation.y][hatLocation.x] = hat;
+        return field;
+    }
+    runGame() {
+        clear();
+        let inGame = true;
+        while (inGame) {
+            this.print();
+            this.askQuestion();
+            if (!this.inBox()) {
+                console.log("Out of box! ~~ GAME OVER!!");
+                inGame = false;
+                break;
+            } else if (this.isHole()) {
+                console.log("Sad, you fell into a hole! ~~ GAME OVER!!");
+                inGame = false;
+                break;
+            } else if (this.isHat()) {
+                console.log("Congratulation, you have found your hat! ~~ You WIN!");
+                inGame = false;
+                break;
+            }
+            this.field[this.locationY][this.locationX] = pathCharacter;
+        }
+    }
+    askQuestion() {
+        const answer = prompt("Which way do you want to go?").toUpperCase();
+        switch (answer) {
+            case "W":
+                this.locationY -= 1;
+                break; 
+            case "S":
+                this.locationY += 1;
+                break;
+            case "A":
+                this.locationX -= 1;
+                break;
+            case "D":
+                this.locationX += 1;
+                break;
+            case "RESET":
+                inGame = false;
+                myField.runGame();
+                break;
+            default:
+                console.log("Enter W = UP, S = DOWN, A = LEFT, D = RIGHT & RESET to Restart");
+                this.askQuestion();
+                break;
+        }
+    }
+    inBox() {
+        return (
+            this.locationY >= 0 &&
+            this.locationX >= 0 &&
+            this.locationY < this.field.length &&
+            this.locationX < this.field[0].length
+        );
+    }
+    isHat() {
+        return this.field[this.locationY][this.locationX] == hat;
+    }
+    isHole() {
+        return this.field[this.locationY][this.locationX] == hole;
+    }
+    print() {
+        clear();
+        const displayString = this.field.map(row => {
+            return row.join('');
+        }).join('\n');
+        console.log(displayString);
+    }
+}
+const myField = new Field(Field.generateField(10, 10, 0.2));
+myField.runGame();
